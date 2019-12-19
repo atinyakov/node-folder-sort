2d5e6357b25619b562af374f89b4299e79094136
atinyakov <akoff16@gmail.com>
2019-12-20 00:53:40 +0300

    remove syncs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const createDir = dest => {
-  fs.mkdirSync(path.join(__dirname, dest), { recursive: true }, err => {
+  fs.mkdir(path.join(__dirname, dest), { recursive: true }, err => {
     if (err) {
       console.log(err);
     } else {
@@ -18,41 +18,79 @@ const getFiles = (src, dest) => {
     }
 
     files.forEach(function(file) {
-      const isDirectory = fs.statSync(path.join(src, `${file}`)).isDirectory();
+      let isDirectory;
+      fs.stat(path.join(src, `${file}`), function(err, stats) {
+        isDirectory = stats.isDirectory();
 
-      if (!isDirectory) {
-        if (!fs.existsSync(`${dest}/${file.toString()[0]}`)) {
-          createDir(`${dest}/${file.toString()[0]}`);
-        }
-        fs.writeFile(
-          path.join(__dirname, "dist", file.toString()[0], file.toString()),
-          file,
-          err => {
-            if (err) {
-              console.log(err);
-            }
+        if (!isDirectory) {
+          if (!fs.existsSync(`${dest}/${file.toString()[0]}`)) {
+            createDir(`${dest}/${file.toString()[0].toUpperCase()}`);
           }
-        );
-      } else {
-        getFiles(path.join(src, `${file}`), dest);
-      }
+          fs.writeFile(
+            path.join(__dirname, "dist", file.toString()[0], file.toString()),
+            file,
+            err => {
+              if (err) {
+                console.log(err);
+              }
+            }
+          );
+        } else {
+          getFiles(path.join(src, `${file}`), dest);
+        }
+      });
     });
   });
 };
 
 const deleteFolderRecursive = function(src) {
+//   console.log(src);
   if (fs.existsSync(src)) {
-    fs.readdirSync(src).forEach((file, index) => {
-      const cursrc = path.join(src, file);
-      if (fs.lstatSync(cursrc).isDirectory()) {
-        // recurse
-        deleteFolderRecursive(cursrc);
-      } else {
-        // delete file
-        fs.unlinkSync(cursrc);
-      }
+    fs.readdir(src, (err, files) => {
+        if(files) {
+            console.log(files)
+        }
+      files.forEach((file, index) => {
+        const cursrc = path.join(src, file);
+        let isDirectory;
+        fs.lstat(cursrc, (err, stats) => {
+          isDirectory = stats.isDirectory();
+            console.log(isDirectory);
+          if (!isDirectory) {
+            // console.log(cursrc.length)
+
+            fs.unlink(cursrc, err => {
+              if (err) throw err;
+            });
+          }
+          if (isDirectory){
+            fs.readdir(cursrc, (err, files) => {
+              console.log(cursrc, isDirectory, files, files.length);
+              if (files.length) {
+                deleteFolderRecursive(cursrc);
+              } else {
+                fs.rmdir(file, err => {
+                    if (err) throw err;
+                });
+              }
+            })
+            // });
+            // delete file
+            // fs.rmdir(path.join(src), err => {
+            //   if (err) throw err;
+            // });
+            //   }
+          }
+        });
+      });
+    //   fs.rmdir(src, err => {
+    //     if (err) throw err;
+    //   });
     });
-    fs.rmdirSync(src);
+//   }
+    // fs.rmdir(path.join(src), err => {
+    //   if (err) throw err;
+    // });
   }
 };
 
@@ -63,7 +101,7 @@ const sort = (src, dist, deletsrc = false) => {
   getFiles(directoryPath, dist);
 
   if (deletsrc) {
-    deleteFolderRecursive(src);
+    deleteFolderRecursive(path.join(__dirname, src));
   }
 };
 
