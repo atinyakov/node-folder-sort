24f86c873b4ea530526d28932ed22c4b62d6da9a
atinyakov <akoff16@gmail.com>
2019-12-30 01:39:33 +0300

    async done

--- async.js
+++ async.js
@@ -11,43 +11,72 @@ const createDir = dest => {
   });
 };
 
-const getFiles = (src, dest) => {
-  fs.readdir(src, function(err, files) {
-    if (err) {
-      return console.log("Unable to scan directory: " + err);
-    }
+const allfiles = []
 
-    files.forEach(function(file) {
-      let isDirectory;
-      fs.stat(path.join(src, `${file}`), function(err, stats) {
-        isDirectory = stats.isDirectory();
+const getFiles = async (src, dest) => {
+  const files = await readdir(src);
+  
+  files.forEach(async function(file) {
+    let isDirectory;
+      const stats = await stat(src, file);
 
-        if (!isDirectory) {
-          if (!fs.existsSync(`${dest}/${file.toString()[0]}`)) {
-            createDir(`${dest}/${file.toString()[0].toUpperCase()}`);
-          }
-          fs.writeFile(
-            path.join(__dirname, "dist", file.toString()[0], file.toString()),
-            file,
-            err => {
-              if (err) {
-                console.log(err);
-              }
-            }
-          );
-        } else {
-          getFiles(path.join(src, `${file}`), dest);
+      isDirectory = stats.isDirectory();
+
+      if (!isDirectory) {
+        if (!fs.existsSync(`${dest}/${file.toString()[0]}`)) {
+          createDir(`${dest}/${file.toString()[0].toUpperCase()}`);
         }
-      });
+
+        allfiles.push(copyFiles(file))
+        
+
+      } else {
+        getFiles(path.join(src, `${file}`), dest);
+      }
+  });
+};
+
+const readdir = src => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(src, function(err, files) {
+      if (err) {
+        return console.log("Unable to scan directory: " + err);
+      } else resolve(files);
     });
   });
 };
 
+const copyFiles = (file) => {
+  return new Promise(resolve => {
+    fs.writeFile(
+      path.join(__dirname, "dist", file.toString()[0], file.toString()),
+      file,
+      err => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+
+    resolve()
+  })
+}
+
+const stat = (src, file) => {
+  return new Promise((resolve, reject) => {
+    fs.stat(path.join(src, `${file}`), function(err, stats) {
+      if (err) {
+        console.log(err);
+      }
+      else resolve (stats)
+    })
+  })
+};
+
 const deleteFolderRecursive = function(src) {
-//   console.log(src);
+  //   console.log(src);
   if (fs.existsSync(src)) {
     fs.readdir(src, (err, files) => {
-
       files.forEach((file, index) => {
         const cursrc = path.join(src, file);
         let isDirectory;
@@ -55,22 +84,21 @@ const deleteFolderRecursive = function(src) {
           isDirectory = stats.isDirectory();
 
           if (!isDirectory) {
-
             fs.unlink(cursrc, err => {
               if (err) throw err;
             });
           }
-          if (isDirectory){
+          if (isDirectory) {
             fs.readdir(cursrc, (err, files) => {
               // console.log(cursrc, isDirectory, files, files.length);
               if (files.length) {
                 deleteFolderRecursive(cursrc);
               } else {
                 fs.rmdir(file, err => {
-                    if (err) throw err;
+                  if (err) throw err;
                 });
               }
-            })
+            });
           }
         });
       });
@@ -81,21 +109,25 @@ const deleteFolderRecursive = function(src) {
 const sort = (src, dist, deletsrc = false) => {
   return new Promise(resolve => {
     const directoryPath = path.join(__dirname, src);
-  
-     createDir(dist);
-     getFiles(directoryPath, dist);
-  
-    if (deletsrc) {
-       deleteFolderRecursive(path.join(__dirname, src));
-    }
 
-    resolve();
-  })
+    createDir(dist);
+    getFiles(directoryPath, dist);
+
+    Promise.all(allfiles).then(() => {
+      console.log('copied')
+    })
+    process.on("exit", () => {
+      if (deletsrc) {
+        deleteFolderRecursive(path.join(__dirname, src));
+      }
+
+      resolve();
+    });
+  });
 };
 
-
-sort('src', 'dist', true).then(function(){
+sort("src", "dist", false).then(function() {
   console.log(`‘Success’`);
-})
+});
 
 module.exports = sort;
